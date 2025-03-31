6301d7f8c58a28f9cdacd14274d4d75b6d9a3857
Ayush Lochan <139963233+AyushLochan@users.noreply.github.com>
2025-03-31 12:10:13 +0530

    Update app.py
    Added function to check its SLR or not .

--- app.py
+++ app.py
@@ -15,15 +15,17 @@ def grammarAugmentation(rules, nonterm_userdef, start_symbol):
         rhs = rhs.strip()
         multirhs = rhs.split('|')
         for rhs1 in multirhs:
-            rhs1 = rhs1.strip().split()
-            rhs1.insert(0, '.')
-            newRules.append([lhs, rhs1])
+            rhs1 = rhs1.strip()
+            if rhs1 == '#':
+                rhs_split = []
+            else:
+                rhs_split = rhs1.split()
+            rhs_split.insert(0, '.')
+            newRules.append([lhs, rhs_split])
     return newRules
 
 def findClosure(input_state, dotSymbol, separatedRulesList, init_start_symbol):
-    """Compute the closure for a given set of items."""
     closureSet = []
-
     if dotSymbol == init_start_symbol:
         for rule in separatedRulesList:
             if rule[0] == dotSymbol:
@@ -37,21 +39,19 @@ def findClosure(input_state, dotSymbol, separatedRulesList, init_start_symbol):
         tempClosureSet = []
         for rule in closureSet:
             indexOfDot = rule[1].index('.')
-            if rule[1][-1] != '.':
+            if indexOfDot < len(rule[1]) - 1:
                 dotPointsHere = rule[1][indexOfDot + 1]
                 for in_rule in separatedRulesList:
-                    if dotPointsHere == in_rule[0] and in_rule not in tempClosureSet:
+                    if dotPointsHere == in_rule[0] and in_rule not in tempClosureSet and in_rule not in closureSet:
                         tempClosureSet.append(in_rule)
-        for rule in tempClosureSet:
-            if rule not in closureSet:
-                closureSet.append(rule)
+        closureSet.extend(tempClosureSet)
     return closureSet
 
 def compute_GOTO(state, statesDict, stateMap, separatedRulesList):
     generateStatesFor = []
     for rule in statesDict[state]:
-        if rule[1][-1] != '.':
-            indexOfDot = rule[1].index('.')
+        indexOfDot = rule[1].index('.')
+        if indexOfDot < len(rule[1]) - 1:
             dotPointsHere = rule[1][indexOfDot + 1]
             if dotPointsHere not in generateStatesFor:
                 generateStatesFor.append(dotPointsHere)
@@ -63,32 +63,23 @@ def GOTO(state, charNextToDot, statesDict, stateMap, separatedRulesList):
     newState = []
     for rule in statesDict[state]:
         indexOfDot = rule[1].index('.')
-        if rule[1][-1] != '.':
-            if rule[1][indexOfDot + 1] == charNextToDot:
-                shiftedRule = copy.deepcopy(rule)
+        if indexOfDot < len(rule[1]) - 1 and rule[1][indexOfDot + 1] == charNextToDot:
+            shiftedRule = copy.deepcopy(rule)
+            shiftedRule[1][indexOfDot], shiftedRule[1][indexOfDot + 1] = shiftedRule[1][indexOfDot + 1], shiftedRule[1][indexOfDot]
+            newState.append(shiftedRule)
 
-                shiftedRule[1][indexOfDot] = shiftedRule[1][indexOfDot + 1]
-                shiftedRule[1][indexOfDot + 1] = '.'
-                newState.append(shiftedRule)
-
-    addClosureRules = []
-    for rule in newState:
-        indexDot = rule[1].index('.')
-        if rule[1][-1] != '.':
-            closureRes = findClosure(newState, rule[1][indexDot + 1], separatedRulesList, None)
-            for r in closureRes:
-                if r not in addClosureRules and r not in newState:
-                    addClosureRules.append(r)
-    newState.extend(addClosureRules)
+    closure = findClosure(newState, charNextToDot, separatedRulesList, None)
+    for item in closure:
+        if item not in newState:
+            newState.append(item)
 
     stateExists = -1
-    for state_num, st_state in statesDict.items():
-        if st_state == newState:
+    for state_num in statesDict:
+        if statesDict[state_num] == newState:
             stateExists = state_num
             break
-
     if stateExists == -1:
-        new_index = max(statesDict.keys()) + 1
+        new_index = max(statesDict.keys()) + 1 if statesDict else 0
         statesDict[new_index] = newState
         stateMap[(state, charNextToDot)] = new_index
     else:
@@ -106,88 +97,58 @@ def generateStates(statesDict, stateMap, separatedRulesList):
                 compute_GOTO(key, statesDict, stateMap, separatedRulesList)
 
 def first(rule, diction, term_userdef):
-    if rule and rule[0]:
-        if rule[0] in term_userdef:
-            return rule[0]
-        elif rule[0] == '#':
-            return '#'
-    if rule:
-        if rule[0] in diction:
-            fres = []
-            rhs_rules = diction[rule[0]]
-            for itr in rhs_rules:
-                indivRes = first(itr, diction, term_userdef)
-                if isinstance(indivRes, list):
-                    fres.extend(indivRes)
-                else:
-                    fres.append(indivRes)
-            if '#' not in fres:
-                return fres
+    if not rule:
+        return ['#']
+    first_symbol = rule[0]
+    if first_symbol in term_userdef:
+        return [first_symbol]
+    elif first_symbol == '#':
+        return ['#']
+    elif first_symbol in diction:
+        fres = []
+        for production in diction[first_symbol]:
+            prod_first = first(production, diction, term_userdef)
+            fres.extend(prod_first)
+        if '#' in fres:
+            fres.remove('#')
+            if len(rule) > 1:
+                rest_first = first(rule[1:], diction, term_userdef)
+                fres.extend(rest_first)
             else:
-                newList = []
-                fres.remove('#')
-                if len(rule) > 1:
-                    ansNew = first(rule[1:], diction, term_userdef)
-                    if ansNew:
-                        if isinstance(ansNew, list):
-                            newList = fres + ansNew
-                        else:
-                            newList = fres + [ansNew]
-                    else:
-                        newList = fres
-                    return newList
                 fres.append('#')
-                return fres
+        return list(set(fres))
+    return []
 
 def follow(nt, diction, start_symbol, term_userdef):
     solset = set()
     if nt == start_symbol:
         solset.add('$')
     for curNT in diction:
-        rhs = diction[curNT]
-        for subrule in rhs:
-            if nt in subrule:
-                while nt in subrule:
-                    index_nt = subrule.index(nt)
-                    subrule = subrule[index_nt + 1:]
-                    if subrule:
-                        res = first(subrule, diction, term_userdef)
-                        if '#' in (res if isinstance(res, list) else [res]):
-                            newList = []
-                            if isinstance(res, list):
-                                res_list = res.copy()
-                                res_list.remove('#')
-                            else:
-                                res_list = [res]
-                            ansNew = follow(curNT, diction, start_symbol, term_userdef)
-                            if ansNew:
-                                if isinstance(ansNew, list):
-                                    newList = res_list + ansNew
-                                else:
-                                    newList = res_list + [ansNew]
-                            else:
-                                newList = res_list
-                            res = newList
-                    else:
-
-                        if nt != curNT:
-                            res = follow(curNT, diction, start_symbol, term_userdef)
-                    if res:
-                        if isinstance(res, list):
-                            for g in res:
-                                solset.add(g)
+        for prod in diction[curNT]:
+            for i, symbol in enumerate(prod):
+                if symbol == nt:
+                    next_pos = i + 1
+                    while next_pos <= len(prod):
+                        if next_pos == len(prod):
+                            if curNT != nt:
+                                solset.update(follow(curNT, diction, start_symbol, term_userdef))
+                            break
+                        next_symbol = prod[next_pos]
+                        first_next = first([next_symbol], diction, term_userdef)
+                        if '#' in first_next:
+                            solset.update([x for x in first_next if x != '#'])
+                            next_pos += 1
                         else:
-                            solset.add(res)
+                            solset.update(first_next)
+                            break
     return list(solset)
 
 def createParseTable(statesDict, stateMap, T, NT, separatedRulesList, rules, diction, term_userdef, start_symbol):
-    """Create the SLR(1) parsing table with SHIFT, GOTO, REDUCE, and ACCEPT actions."""
     rows = list(statesDict.keys())
     cols = T + ['$'] + NT
     Table = []
     for _ in rows:
         Table.append([''] * len(cols))
-
 
     for entry in stateMap:
         state = entry[0]
@@ -195,17 +156,16 @@ def createParseTable(statesDict, stateMap, T, NT, separatedRulesList, rules, dic
         a = rows.index(state)
         b = cols.index(symbol)
         if symbol in NT:
-
             Table[a][b] += f"{stateMap[entry]} "
         elif symbol in T:
-
             Table[a][b] += f"S{stateMap[entry]} "
 
     numbered = {}
     key_count = 0
     for rule in separatedRulesList:
         tempRule = copy.deepcopy(rule)
-        tempRule[1].remove('.')
+        if '.' in tempRule[1]:
+            tempRule[1].remove('.')
         numbered[key_count] = tempRule
         key_count += 1
 
@@ -214,17 +174,16 @@ def createParseTable(statesDict, stateMap, T, NT, separatedRulesList, rules, dic
     rules_copy.insert(0, addedR)
 
     for rule in rules_copy:
-        k = rule.split("->")
-        lhs = k[0].strip()
-        rhs = k[1].strip()
-        multirhs = [r.strip().split() for r in rhs.split('|')]
-        diction[lhs] = multirhs
+        lhs, rhs = rule.split("->")
+        lhs = lhs.strip()
+        rhs = [r.strip().split() for r in rhs.strip().split('|')]
+        diction[lhs] = [['#'] if part == [''] else part for part in rhs]
 
     for stateno in statesDict:
         for rule in statesDict[stateno]:
-            if rule[1][-1] == '.':
+            if rule[1][-1] == '.' or (len(rule[1]) == 1 and rule[1][0] == '.'):
                 temp2 = copy.deepcopy(rule)
-                temp2[1].remove('.')
+                temp2[1] = [x for x in temp2[1] if x != '.']
                 for key in numbered:
                     if numbered[key] == temp2:
                         follow_result = follow(rule[0], diction, start_symbol, term_userdef)
@@ -234,8 +193,30 @@ def createParseTable(statesDict, stateMap, T, NT, separatedRulesList, rules, dic
                                 if key == 0:
                                     Table[stateno][index] = "Accept"
                                 else:
-                                    Table[stateno][index] += f"R{key} "
+                                    Table[stateno][index] += f"R{key} " if Table[stateno][index] != "Accept" else f"R{key} "
+
     return Table, rows, cols
+
+
+def generate_conflict_counts(table):
+    sr = 0
+    rr = 0
+
+    for row in table:
+        for cell in row:
+            if cell and cell != 'ac':
+                actions = cell.split()
+                shift_count = sum(1 for action in actions if action.startswith('S'))
+                reduce_count = sum(1 for action in actions if action.startswith('R'))
+
+                if shift_count > 0 and reduce_count > 0:
+                    sr += 1
+                elif reduce_count > 1:
+                    rr += 1
+
+    return sr, rr
+
+
 
 def main():
     st.set_page_config(page_title="SLR(1) Parser Generator", layout="wide")
@@ -255,7 +236,6 @@ F -> ( E ) | id"""
         generate_btn = st.button("Generate SLR(1) Parsing Table")
     
     if generate_btn:
-
         rules = [line.strip() for line in grammar_input.splitlines() if line.strip()]
         nonterm_userdef = [x.strip() for x in nonterm_input.split(',')]
         term_userdef = [x.strip() for x in term_input.split(',')]
@@ -266,33 +246,34 @@ F -> ( E ) | id"""
             st.write(rule)
 
         separatedRulesList = grammarAugmentation(rules, nonterm_userdef, start_symbol)
-        st.subheader("Grammar after Augmentation:")
-        aug_output = "\n".join([f"{rule[0]} -> {' '.join(rule[1])}" for rule in separatedRulesList])
+        st.subheader("Augmented Grammar:")
+        aug_output = "\n".join([f"{rule[0]} -> {' '.join(rule[1]) if rule[1] else 'ε'}" for rule in separatedRulesList])
         st.text(aug_output)
 
-        I0 = findClosure(0, separatedRulesList[0][0], separatedRulesList, separatedRulesList[0][0])
-        st.subheader("Calculated Closure: I0")
-        closure_output = "\n".join([f"{rule[0]} -> {' '.join(rule[1])}" for rule in I0])
+        init_start_symbol = separatedRulesList[0][0]
+        I0 = findClosure([separatedRulesList[0]], separatedRulesList[0][1][1], separatedRulesList, init_start_symbol)
+        st.subheader("Initial Closure (I0):")
+        closure_output = "\n".join([f"{rule[0]} -> {' '.join(rule[1]) if rule[1] else 'ε'}" for rule in I0])
         st.text(closure_output)
 
-        statesDict = {}
+        statesDict = {0: I0}
         stateMap = {}
-        statesDict[0] = I0
         generateStates(statesDict, stateMap, separatedRulesList)
-        
+
         st.subheader("Generated States:")
         states_output = ""
         for st_num, state in statesDict.items():
-            states_output += f"I{st_num}:\n"
+            states_output += f"State I{st_num}:\n"
             for rule in state:
-                states_output += f"   {rule[0]} -> {' '.join(rule[1])}\n"
+                rhs = ' '.join(rule[1]) if rule[1] else 'ε'
+                states_output += f"    {rule[0]} → {rhs}\n"
             states_output += "\n"
         st.text(states_output)
-        
-        st.subheader("GOTO Computation:")
+
+        st.subheader("GOTO Transitions:")
         goto_output = ""
-        for key in stateMap:
-            goto_output += f"GOTO(I{key[0]}, {key[1]}) = I{stateMap[key]}\n"
+        for (src, symbol), dest in stateMap.items():
+            goto_output += f"GOTO(I{src}, {symbol}) = I{dest}\n"
         st.text(goto_output)
 
         diction = {}
@@ -311,6 +292,15 @@ F -> ( E ) | id"""
         st.subheader("SLR(1) Parsing Table:")
         df = pd.DataFrame(Table, index=[f"I{i}" for i in rowStates], columns=cols)
         st.dataframe(df)
+
+        sr, rr = generate_conflict_counts(Table)
+        st.subheader("Conflict Analysis:")
+        st.markdown(f"**Shift-Reduce Conflicts:** {sr}")
+        st.markdown(f"**Reduce-Reduce Conflicts:** {rr}")
+        if sr == 0 and rr == 0:
+            st.success("The grammar is SLR(1).")
+        else:
+            st.error("The grammar is NOT SLR(1).")
 
 if __name__ == "__main__":
     main()
